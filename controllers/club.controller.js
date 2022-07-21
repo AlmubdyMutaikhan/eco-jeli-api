e91c1b1184e365393ac2351f6e58a836937f6a3e
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-21 17:22:05 +0600

    add club update

--- controllers/club.controller.js
+++ controllers/club.controller.js
@@ -24,7 +24,28 @@ const updateClubStruct = async (req, res) => {
     }
 }
 
+const saveClubData = async (req, res) => {
+    try {
+        const leaderID = req.body.leaderID;
+        const data = req.body.club;
+        
+        await Club.findOneAndUpdate({leader:leaderID}, {$set:
+            {
+                city : data.city,
+                name:data.name,
+                logo:data.logo,
+                description:data.description,
+        }});
+        res.status(201).send({"msg":"ok"});
+    } catch(err) {
+        console.log(err);
+        res.status(400).send({"msg":"nok", "error" : err.message});
+    }
+}
+
+
 module.exports = {
     retrieveClubData,
-    updateClubStruct
+    updateClubStruct,
+    saveClubData
 }
--- routes/club.route.js
+++ routes/club.route.js
@@ -4,4 +4,6 @@ const clubRoute = Router();
 
 clubRoute.get('/', ClubController.retrieveClubData);
 clubRoute.put('/', ClubController.updateClubStruct);
+clubRoute.post('/', ClubController.saveClubData);
+
 module.exports = clubRoute;
