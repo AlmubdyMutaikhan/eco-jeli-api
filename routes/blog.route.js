869d64f088dd8e956a393a5cb5dc2cb7bb8f7e04
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-23 18:17:10 +0600

    change uri

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -2,10 +2,11 @@ const {Router} = require('express');
 const blogRoute = Router();
 const blogController = require('../controllers/blog.controller');
 
+blogRoute.get('/all/blogs', blogController.getAllBlogs);
 blogRoute.delete('/:blogID', blogController.checkAuth, blogController.deleteBlog);
 blogRoute.put('/:blogID', blogController.checkAuth, blogController.updateBlog);
 blogRoute.get('/:blogID', blogController.checkAuth, blogController.getBlog);
 blogRoute.post('/new', blogController.checkAuth, blogController.postBlog);
-blogRoute.get('/all', blogController.getAllBlogs);
+
 
 module.exports = blogRoute;
