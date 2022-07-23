0f877fabb5fdd00b8c0a2fcc507306ad29186daa
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-23 18:14:31 +0600

    add

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -2,11 +2,10 @@ const {Router} = require('express');
 const blogRoute = Router();
 const blogController = require('../controllers/blog.controller');
 
-
-blogRoute.post('/new', blogController.checkAuth, blogController.postBlog);
 blogRoute.delete('/:blogID', blogController.checkAuth, blogController.deleteBlog);
 blogRoute.put('/:blogID', blogController.checkAuth, blogController.updateBlog);
 blogRoute.get('/:blogID', blogController.checkAuth, blogController.getBlog);
+blogRoute.post('/new', blogController.checkAuth, blogController.postBlog);
 blogRoute.get('/all', blogController.getAllBlogs);
 
 module.exports = blogRoute;
