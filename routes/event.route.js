d8b7cba72ee3237bac88dd2aeb96d752aa5eadb3
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-24 01:58:38 +0600

    order routes

--- routes/event.route.js
+++ routes/event.route.js
@@ -3,10 +3,10 @@ const eventRoute = Router();
 const eventController = require('../controllers/event.controller');
 const authMid = require('../middleware/auth.mid');
 
-
+eventRoute.get('/all', eventController.getAllEvents);
 eventRoute.post('/new', authMid.checkAuth, eventController.postEvent);
 eventRoute.delete('/:eventID', authMid.checkAuth, eventController.deleteEvent);
 eventRoute.put('/:eventID', authMid.checkAuth, eventController.updateEvent);
 eventRoute.get('/:blogID', authMid.checkAuth, eventController.getEvent);
-eventRoute.get('/all', eventController.getAllEvents);
+
 module.exports = eventRoute;
