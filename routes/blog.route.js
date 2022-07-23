f2cff6933bd73792179da358a4ea83a221c667d3
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2022-07-23 20:52:49 +0600

    fix bugs

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,9 +3,11 @@ const blogRoute = Router();
 const blogController = require('../controllers/blog.controller');
 
 blogRoute.get('/all/blogs', blogController.getAllBlogs);
+blogRoute.get('/:blogID', blogController.getBlog);
+
 blogRoute.delete('/:blogID', blogController.checkAuth, blogController.deleteBlog);
 blogRoute.put('/:blogID', blogController.checkAuth, blogController.updateBlog);
-blogRoute.get('/:blogID', blogController.checkAuth, blogController.getBlog);
+
 blogRoute.post('/new', blogController.checkAuth, blogController.postBlog);
 
 
